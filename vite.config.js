92d1e176204685b2b00267c1f6064c67864f7e40
Nestl706 <nestl.skitek@gost.bic-lj.si>
2025-11-05 18:04:39 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// 👇 DODAJ TO KONFIGURACIJO
 export default defineConfig({
   plugins: [react()],
+  root: '.', // pove Vite, da je index.html v root mapi
+  build: {
+    outDir: 'dist'
+  }
 })
